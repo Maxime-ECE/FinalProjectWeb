1a1bae6a19f3626a1020571e7a3323e255979ba8
Yanis <yanis.gerard@edu.ece.fr>
2024-05-06 21:45:23 +0200

    dependency

--- back-skeleton/src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
+++ back-skeleton/src/main/java/com/takima/backskeleton/BackSkeletonApplication.java
@@ -3,7 +3,7 @@ package com.takima.backskeleton;
 import com.takima.backskeleton.models.Users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class BackSkeletonApplication {
