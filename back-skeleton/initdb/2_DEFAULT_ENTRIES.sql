61e449956266198b81c7e2706a2dde345239908d
Maxime-ECE <maximeperrier21@gmail.com>
2024-05-13 00:42:43 +0200

    data for quizz

--- back-skeleton/initdb/2_DEFAULT_ENTRIES.sql
+++ back-skeleton/initdb/2_DEFAULT_ENTRIES.sql
@@ -45,12 +45,37 @@ VALUES
     (17, 'Quel est le plus grand océan sur Terre?', 'Océan Pacifique', 'Océan Atlantique,Océan Indien,Océan Arctique,Océan Pacifique', 'Couvre plus de surface que tous les continents réunis'),
     (18, 'Qui a peint le plafond de la Chapelle Sixtine?', 'Michel-Ange', 'Léonard de Vinci,Raphaël,Donatello,Michel-Ange', 'A créé des fresques célèbres'),
     (19, 'Quel est le symbole chimique du dioxyde de carbone?', 'CO2', 'CO,CO2,O2,N2', 'Produit lors de la respiration et de la combustion'),
-    (20, 'Qui a été la première femme à remporter un prix Nobel?', 'Marie Curie', 'Rosalind Franklin,Marie Curie,Dorothy Crowfoot Hodgkin,Gerty Cori', 'Récompensée pour la physique en 1903 et la chimie en 1911');
-
+    (20, 'Qui a été la première femme à remporter un prix Nobel?', 'Marie Curie', 'Rosalind Franklin,Marie Curie,Dorothy Crowfoot Hodgkin,Gerty Cori', 'Récompensée pour la physique en 1903 et la chimie en 1911'),
+    (21, 'Quel est le symbole chimique de loxygène?', 'O', 'O, H, C, N', 'Essentiel à la respiration'),
+    (22, 'Combien de continents y a-t-il sur Terre?', '7', '5, 6, 7, 8', 'Ils comprennent lAmérique, lEurope, lAsie, lAfrique, lOcéanie, lAntarctique et lAtlantide'),
+    (23, 'Quel est le plus grand désert du monde?', 'Antarctique', 'Sahara, Gobi, Antarctique, Kalahari', 'Une étendue glacée et sèche'),
+    (24, 'Qui a écrit 1984?', 'George Orwell', 'George Orwell, Aldous Huxley, J.R.R. Tolkien, F. Scott Fitzgerald', 'Une dystopie célèbre'),
+    (25, 'Quelle est la capitale du Japon?', 'Tokyo', 'Pékin, Séoul, Tokyo, Bangkok', 'Célèbre pour sa technologie et sa culture'),
+    (26, 'Quel est le plus long fleuve du monde?', 'Nil', 'Nil, Amazone, Mississippi, Yangtsé', 'Coulant à travers lAfrique'),
+    (27, 'Quelle est la plus haute montagne du monde?', 'Mont Everest', 'Mont Everest, K2, Kilimandjaro, Mont Fuji', 'Situé dans lHimalaya'),
+    (28, 'Quel est le symbole chimique du sodium?', 'Na', 'Na, So, Si, Sd', 'Réactif et métallique'),
+    (29, 'Qui a peint La Nuit étoilée?', 'Vincent van Gogh', 'Vincent van Gogh, Pablo Picasso, Claude Monet, Leonardo da Vinci', 'Connue pour son utilisation distinctive de la couleur'),
+    (30, 'Quel est le composé chimique principal des os humains?', 'Calcium', 'Calcium, Fer, Sodium, Potassium', 'Contribue à la structure et à la force des os'),
+    (31, 'Quel est le plus grand animal terrestre?', 'Éléphant africain', 'Éléphant africain, Girafe, Rhinocéros blanc, Hippopotame', 'Caractérisé par sa trompe distinctive'),
+    (32, 'Quel pays est le plus peuplé du monde?', 'Chine', 'Inde, Chine, États-Unis, Indonésie', 'A une population de plus dun milliard'),
+    (33, 'Quel est lélément chimique le plus abondant dans lunivers observable?', 'Hydrogène', 'Hydrogène, Hélium, Oxygène, Carbone', 'Composant principal des étoiles'),
+    (34, 'Quel est le plus grand lac deau douce du monde en superficie?', 'lac Supérieur', 'Lac Supérieur, lac Baïkal, lac Victoria, lac Tanganyika', 'Partage ses frontières avec les États-Unis et le Canada'),
+    (35, 'Quel est le nombre dos dans le corps humain adulte?', '206', '206, 215, 192, 248', 'Comprend des os longs, courts, plats et irréguliers'),
+    (36, 'Qui a écrit Le Petit Prince?', 'Antoine de Saint-Exupéry', 'Antoine de Saint-Exupéry, J.K. Rowling, Ernest Hemingway, F. Scott Fitzgerald', 'Une histoire poétique et philosophique'),
+    (37, 'Quel est le symbole chimique de leau de table?', 'H2O', 'H2O, CO2, NaCl, O2', 'Essentielle à la vie'),
+    (38, 'Quel est le pays le plus grand par superficie terrestre?', 'Russie', 'Russie, Canada, Chine, États-Unis', 'Couvrant une grande partie de lAsie et de lEurope'),
+    (39, 'Qui a écrit Orgueil et Préjugés?', 'Jane Austen', 'Jane Austen, Charlotte Brontë, Emily Brontë, Louisa May Alcott', 'Un roman classique de la littérature anglaise'),
+    (40, 'Quel est le nom du robot explorateur de Mars de la NASA?', 'Perseverance', 'Curiosity, Perseverance, Opportunity, Spirit', 'Recherche de signes de vie passée sur Mars');
 INSERT INTO quizz (titre, question_id,image)
-VALUES ('Mon-super-quizz', '1,2,3,4,5,6,7,8,9,10', null);
+VALUES ('Quizz-1', '1,2,3,4,5,6,7,8,9,10', null);
 INSERT INTO quizz (titre, question_id, image)
-VALUES ('Autre-quizz', '11,12,13,14,15,16,17,18,19,20', null);
+VALUES ('Quizz-2', '11,12,13,14,15,16,17,18,19,20', null);
+INSERT INTO quizz (titre, question_id, image)
+VALUES ('Quizz-3', '21,22,23,24,25,26,27,28,29,30', null);
+
+INSERT INTO quizz (titre, question_id, image)
+VALUES ('Quizz-4', '31,32,33,34,35,36,37,38,39,40', null);
+
 
 CREATE SEQUENCE ma_sequence;
 ALTER SEQUENCE ma_sequence RESTART 10000 increment by 50;
